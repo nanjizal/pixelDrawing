88f52bcff99b671d8c7587556429c9ce827770db
nanjizal <nitsuj@nanjizal.net>
2017-07-03 11:46:13 +0100

    Delete IPixelShape.hx

--- pixelDrawing/IPixelShape.hx
+++
@@ -1,9 +0,0 @@
-package pixelDrawing;
-
-interface IPixelShape {
-    function hitTest( x: Float, y: Float ): Bool;
-    function fill( col: Int, alpha: Float ):Void;
-    function plot( col: Int, alpha: Float, ?wid: Float = 1 ): Void;
-    function move( x: Float, y: Float ):Void;
-    function translate( x: Float, y: Float ):Void;
-}
