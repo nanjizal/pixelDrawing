0c805081bb854e1f2dede2005302016e250e1714
nanjizal <none>
2020-07-09 15:30:43 +0100

    add dox

---
+++ src/pixelDrawing/upkeep/Doc.hx
@@ -0,0 +1,21 @@
+package pixelDrawing.upkeep;
+import pixelDrawing.Circle;
+import pixelDrawing.Cubic;
+import pixelDrawing.Fill;
+import pixelDrawing.IPixelShape;
+import pixelDrawing.Line;
+import pixelDrawing.Plot;
+import pixelDrawing.Quadratic;
+import pixelDrawing.Rectangle;
+import pixelDrawing.ShapeEnum;
+import pixelDrawing.Triangle;
+
+@:dox(hide)
+class Doc {
+    public static function main(){
+        
+    }
+    public function new(){
+        
+    }
+}
